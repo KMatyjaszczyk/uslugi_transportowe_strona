e1611df3cb4b8d1250a9448bf1591f35f2ed2b48
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2021-12-29 11:14:30 +0100

    Change db configuration, begin writing user repository

--- classes/repository/DbConfiguration.php
+++ classes/repository/DbConfiguration.php
@@ -1,2 +1,8 @@
 <?php
 // TODO write class
+class DbConfiguration {
+    public static string $DB_ADDRESS = 'localhost';
+    public static string $DB_USER = 'root';
+    public static string $DB_PASSWORD = '';
+    public static string $DB_DATABASE = 'uslugi_transportowe';
+}
